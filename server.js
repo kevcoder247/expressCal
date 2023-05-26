13319031faa2b495d1b8d6ce88f0ba37a900a32a
kevin ventura <kevcoder@kevins-mbp.myfiosgateway.com>
2023-05-26 16:10:02 -0400

    wip: we can now by taking the routes input and adding them together

--- server.js
+++ server.js
@@ -2,9 +2,22 @@ const express = require('express');
 const app = express();
 const PORT = 3000;
 
+app.get('/:num1/:num2', (req, res) => {
+  const firstNum = parseInt(req.params.num1);
+  const secondNum = parseInt(req.params.num2);
+  const sum = firstNum + secondNum;
+  console.log(sum)
+  res.send(`<h1>${sum}</h1>`)
+  
+})
 
+app.get('/somerout', (req, res) => {
+  console.log('req.query:', req.query)
+  res.send('someroute accessed')
+}) 
 
 
 app.listen(PORT, () => {
   console.log(`Express is listening on port ${PORT}`)
 })
+
