88cf8d5681163d1f2b4d8d35d146c206ec0c4c4b
kevin ventura <kevcoder@kevins-mbp.myfiosgateway.com>
2023-05-26 17:55:25 -0400

    wip: user can now add 2 parameters to the URL and do basic arithmetic

--- server.js
+++ server.js
@@ -2,19 +2,35 @@ const express = require('express');
 const app = express();
 const PORT = 3000;
 
-app.get('/:num1/:num2', (req, res) => {
+app.get("/calcquery/:num1/:num2", (req, res) => {
   const firstNum = parseInt(req.params.num1);
   const secondNum = parseInt(req.params.num2);
-  const sum = firstNum + secondNum;
-  console.log(sum)
-  res.send(`<h1>${sum}</h1>`)
-  
-})
+  const operator = req.query.operation;
+  //console.log(operator);
+  // console.log('req.params', req.params)
+  // console.log(typeof secondNum);
+  if(operator === 'add'){
+    const sum = firstNum + secondNum;
+    console.log(operator)
+    res.send(`Total sum is ${sum}`)
+  }else if(operator === 'subtract'){
+    const sub = firstNum - secondNum; 
+    console.log(operator)
+    res.send(`The Total of your subtraction is ${sub}`)
+  }else if(operator === 'multiply'){
+    const product = firstNum * secondNum;
+    res.send(`The product is ${product}`);
+  }else if(operator === 'divide'){
+    const division = firstNum / secondNum;
+    res.send(`The result of your division is ${division}`)
+  }else if(operator === 'exponent'){
+    const exponent = firstNum ** secondNum;
+    res.send(`The result of your exponent is ${exponent}`)
+  }else{
+    res.send(`No operation`)
+  }
 
-app.get('/somerout', (req, res) => {
-  console.log('req.query:', req.query)
-  res.send('someroute accessed')
-}) 
+})
 
 
 app.listen(PORT, () => {
